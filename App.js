6dd2c711915618edf52b5f752d276b886040d10e
Mai Eisenberg <r0377155@student.thomasmore.be>
2024-08-13 22:55:21 +0200

    bottom bar done

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-// Import screen components
-// import HomePage from "./screens/HomePage";
-// import FavPage from "./screens/FavPage";
-// import Catalogus from "./screens/Catalogus";
-// import ItemPage from "./screens/ItemPage";
-// import TabNav from "./components/TabNav";
 
 function Home() {
   return (
@@ -35,78 +28,58 @@ function FavorPage() {
   );
 }
 
-function HomeScreen ({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello, this is Tennos, and it's all about tennis rackets!</Text>
-      <Text style={styles.text}>More text...</Text>
-      <TextInput style={styles.textInput} placeholder="Text goes here..." />
-      <Image 
-        style={styles.image}
-        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// function HomeScreen ({ navigation }) {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>Hello, this is Tennos, and it's all about tennis rackets!</Text>
+//       <Text style={styles.text}>More text...</Text>
+//       <TextInput style={styles.textInput} placeholder="Text goes here..." />
+//       <Image 
+//         style={styles.image}
+//         source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+//       />
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-function TabNavigator() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Rackets" component={Rackets} />
-      <Tab.Screen name="Favorites" component={FavorPage} />
-    </Tab.Navigator>
-  );
-}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="MainTabs">
-        <Stack.Screen
-          name="MainTabs"
-          component={TabNavigator}
-          options={{ headerShown: false }} // Hide header if not needed
-        />
-        <Stack.Screen
-          name="Rackets"
-          component={HomeScreen}
-          options={{
-            headerStyle: { backgroundColor: "#FFFCF5" },
-            headerTintColor: "#0D0D12",
-            headerTitleAlign: "left",
-            animation: "none",
-          }}
-        />
-      </Stack.Navigator>
+       <Tab.Navigator
+       screenOptions ={({route}) => ({
+        tabBarIcon:  ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home';
+          
+          } else if (route.name === 'Rackets') {
+            iconName = focused ? 'trophy' : 'trophy';
+          } else if (route.name === 'Favorites') { 
+            iconName = focused ? 'heart' : 'heart';
+          }
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+
+        tabBarActiveTintColor: '#D94A01',
+        tabBarInactiveTintColor: 'gray',
+
+       })}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Rackets" component={Rackets} />
+      <Tab.Screen name="Favorites" component={FavorPage} />
+    </Tab.Navigator>
     </NavigationContainer>
+
+    
   );
 }
 
 
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen
-//           name="Rackets"
-//           component={HomeScreen}
-//           options={{
-//             headerStyle: { backgroundColor: "#FFFCF5" },
-//             headerTintColor: "#0D0D12",
-//             headerTitleAlign: "left",
-//             animation: "none",
-//           }}
-//         />
-//       <Stack.Screen name="MainTabs" component={TabNavigator} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
 
 const styles = StyleSheet.create({
   container: {
